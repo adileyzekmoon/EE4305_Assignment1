ef9a3a051768026f446e53bb6f15319dfd30e046
DESKTOP-ISL6DTP\Adil Eyzekmoon <adil.eyzekmoon@gmail.com>
2020-03-16 16:50:11 +0800

    Added report

--- Q1A.py
+++ Q1A.py
@@ -1,7 +1,7 @@
 import sys
 sys.setrecursionlimit(1100)
 
-nu = 0.001 
+n = 0.001 
 
 def derivX(x,y):
     answer = (2*(x-1)) - (400*x*(y-(x**2)))
@@ -14,20 +14,16 @@ def derivY(x,y):
     return(answer)
 
 def deltaX(x,y):
-    answer = x - (nu*derivX(x,y))
+    answer = x - (n*derivX(x,y))
     return answer
 
 def deltaY(x,y):
-    answer = y - (nu*derivY(x,y))
+    answer = y - (n*derivY(x,y))
     return answer
 
 def f(x,y):
     answer = ((1-x)**2) + 100*((y-(x**2))**2)
     return answer
-
-#
-#print(deltaX(0.004316, .32))
-#print(deltaY(0.004316, .32))
 
 def steepestdescent(x,y,n):
     if (f(x,y)<.1):
